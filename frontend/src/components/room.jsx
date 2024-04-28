133a7f4baaa8f66a23979a10c681aaa5cd777bdb
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-04-28 16:16:44 +0200

    [ADD] room page

--- frontend/src/components/room.jsx
+++ frontend/src/components/room.jsx
@@ -5,14 +5,15 @@ const Room = () => {
     <div className="room-container">
       <img src={logo} alt="Tetris Logo" className="logo" />
       <div className="room-box">
-        <form>
-          <label htmlFor="room-name">Room Name</label>
-          <input type="text" placeholder="Room Name" />
-          <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username" />
-          <button type="submit">Create Room</button>
-          <button type="submit">Join Room</button>
-        </form>
+        <div className="room-form">
+          <form>
+            <label htmlFor="room-name">Room Name</label>
+            <input type="text" placeholder="Room Name" />
+            <label htmlFor="username">Username</label>
+            <input type="text" placeholder="Username" />
+            <button type="submit">Create / Join Room</button>
+          </form>
+        </div>
       </div>
     </div>
   );
