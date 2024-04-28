cf6dfcb2654df12cdec87f20a68d6721933b0606
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-04-28 20:39:39 +0200

    [ADD] reduuuuux, join room

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,43 +1,41 @@
 import { Logger } from '@nestjs/common';
 import {
-    SubscribeMessage,
-    WebSocketGateway,
-    OnGatewayInit,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-    WebSocketServer,
-  } from '@nestjs/websockets';
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { CheckIfUserIsUnique, User, registerUser } from 'src/users/users';
-  
-  @WebSocketGateway(4000)
-  export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer()
-    private wss: Server;
-    private logger: Logger = new Logger('AppGateway');
-    private users: User[] = [];
 
+@WebSocketGateway(4001)
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  private wss: Server;
+  private logger: Logger = new Logger('AppGateway');
+  private users: User[] = [];
 
-    afterInit(server: Server) {
-      this.logger.log('Initialized!');
-    }
-    handleDisconnect(client: Socket) {
-      this.logger.log(`Client connected: ${client.id}`);
-    }
-  
-    handleConnection(client: Socket, ...args: any[]) {
-      this.logger.log(`Client disconnected: ${client.id}`);
-    }
-    @SubscribeMessage('joinRoom')
-    handleJoinRoom(client: Socket, room: string, pseudo: string): void {
-        this.logger.log(`Client joined room: ${room} with pseudo : ${pseudo}`);
-        if (CheckIfUserIsUnique(this.users, pseudo) == 1) {
-            client.emit("error", "Error: the pseudo is already used");
-        }
-        registerUser(this.users, pseudo, room, client);
-        client.emit("playerInRoom", )
-    }
-    
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string, pseudo: string): void {
+    this.logger.log(`Client joined room: ${room} with pseudo : ${pseudo}`);
+    if (CheckIfUserIsUnique(this.users, pseudo) == 1) {
+      client.emit('error', 'Error: the pseudo is already used');
+    }
+    registerUser(this.users, pseudo, room, client);
+    client.emit('playerInRoom');
+  }
 }
-  
