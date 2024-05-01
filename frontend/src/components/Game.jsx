10b1005d38bd10e2c587f697698777d2cbd0972e
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-05-01 15:49:39 +0200

    [ADD] game grid OK

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,12 +1,37 @@
 function Game() {
-  const cells = Array.from({ length: 200 }).map((_, index) => (
-    <div key={index}></div>
-  ));
+  const grid = [
+    ["X", "X", "X", "T", "T", "T", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "T", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "S", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "S", "S"],
+    ["X", "X", "L", "Z", "X", "X", "J", "J", "J", "S"],
+    ["L", "L", "L", "Z", "Z", "X", "X", "J", "O", "O"],
+    ["I", "I", "I", "I", "Z", "X", "X", "J", "O", "O"],
+  ];
 
   return (
-    // <div className="room-game">
-    <div className="game-grid">{cells}</div>
-    // </div>
+    <div className="game-container">
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((cell, cellIndex) => (
+            <div key={cellIndex} className={`cell ${cell}`} />
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
