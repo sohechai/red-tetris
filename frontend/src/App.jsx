2d3fecb70e6ad7afae95f82de1577dfb00d48595
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-08-15 19:30:51 +0200

    [ADD] Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Play from "./components/Play.jsx";
+import Home from "./components/Home.jsx";
 import Room from "./components/Room.jsx";
 import RoomName from "./components/RoomName.jsx";
 import Game from "./components/Game.jsx";
@@ -11,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Play />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/room" exact element={<Room />} />
 		  <Route path="/:room/:playerName" element={<RoomName />} />
           <Route path="/game" exact element={<Game />} />
